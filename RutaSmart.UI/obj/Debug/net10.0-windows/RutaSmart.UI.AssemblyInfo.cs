ea1fa9d6bdab699e745e67eba6dbc0eeb11ac0a7
Leander-Valdez <leandervaldezreyes@gmail.com>
2026-04-22 20:03:07 -0400

    Implementacion de Service

--- RutaSmart.UI/obj/Debug/net10.0-windows/RutaSmart.UI.AssemblyInfo.cs
+++ RutaSmart.UI/obj/Debug/net10.0-windows/RutaSmart.UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RutaSmart.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c45b39345e4593bae35d7fa171bd789d4f93ea8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8af2f801d191808309f45e0bd0d1f17b0b21b358")]
 [assembly: System.Reflection.AssemblyProductAttribute("RutaSmart.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RutaSmart.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
