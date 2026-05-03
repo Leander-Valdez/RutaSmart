07cca9d84fe116238aeec1bc397cb55c1aabe633
Leander-Valdez <leandervaldezreyes@gmail.com>
2026-05-03 11:38:31 -0400

    Implementando Ruta Smart

---
+++ RutaSmart/RutaSmart.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace RutaSmart.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
